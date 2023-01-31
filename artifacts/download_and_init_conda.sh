4b8605ca3a16f26d483d5af3da3634b6506375ae
AkumuYuma <manumanu1355@gmail.com>
2023-01-31 20:00:52 +0100

    .

--- artifacts/download_and_init_conda.sh
+++ artifacts/download_and_init_conda.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /miniconda.sh &&
-chmod 755 /miniconda3.sh && 
+chmod 755 /miniconda.sh && 
 /miniconda3.sh -b -p /miniconda &&
 /miniconda/bin/conda init
